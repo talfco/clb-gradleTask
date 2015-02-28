fab8d8874a1eabf04ff4144fdaf644ceec091a12
CurationDockerEngine <felix@cloudburo.net>
2015-02-28 19:48:25 +0100

    Removed hard checks

--- src/main/groovy/com/cloudburo/utility/Cryptor.groovy
+++ src/main/groovy/com/cloudburo/utility/Cryptor.groovy
@@ -1,11 +1,15 @@
 package com.cloudburo.utility
 
+import org.apache.log4j.Logger
+
 // Handles the encryption of passwords stored in the cloud
 class Cryptor   {
 
 	String credentialsPath="src/main/resources/"
 	String sslkey
 	String ssliv
+	
+	static final Logger logger = Logger.getLogger(Cryptor.class)
 	
 	Cryptor () {
 		sslkey = getSSLKey()
@@ -15,21 +19,20 @@ class Cryptor   {
 	String  decrypt(String fileName)  {
 		StringBuffer out = new StringBuffer()
 		def result = Utilities.executeOnShell("openssl des3 -d -K ${sslkey} -iv ${ssliv} -in ${fileName}",new File(credentialsPath),out) 
-		assert result == 0
+		if (result != 0)
+		  logger.warning("Decrypt of '${fileName}' failed")
 		return out.toString()
 	}
 
 	String getSSLKey() {
 		def sslkey = System.getenv('SSL_KEY')
 		if (!sslkey) {	sslkey = System.properties['SSL_KEY'] }
-		assert(sslkey)
 		return sslkey
 	}
 	
 	String getSSLIv() {
 		def ssliv = System.getenv('SSL_IV')
 		if (!ssliv) { ssliv = System.properties['SSL_IV'] }
-		assert (ssliv)
 		return ssliv
 	}
 
