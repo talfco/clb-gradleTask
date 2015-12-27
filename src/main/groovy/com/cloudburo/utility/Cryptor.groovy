c0a525cd280ce7297e3b5ab81ba5763a41840e7b
CurationDockerEngine <felix@cloudburo.net>
2015-12-27 16:27:27 +0100

    Added AWS KMS Encryption

--- src/main/groovy/com/cloudburo/utility/Cryptor.groovy
+++ src/main/groovy/com/cloudburo/utility/Cryptor.groovy
@@ -1,18 +1,49 @@
 package com.cloudburo.utility
 
 import groovy.util.logging.Slf4j
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+import com.amazonaws.services.kms.AWSKMSClient
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.regions.Regions
+import com.amazonaws.regions.Region
+import com.amazonaws.services.kms.model.DecryptRequest
+import com.amazonaws.services.kms.model.DecryptResult
+import com.amazonaws.services.kms.model.EncryptRequest
+import com.amazonaws.services.kms.model.EncryptResult
+
 
 // Handles the encryption of passwords stored in the cloud
 @Slf4j
 class Cryptor   {
-
+	
 	String credentialsPath="src/main/resources/"
 	String sslkey
 	String ssliv
+	AWSKMSClient awsKmsClient = new AWSKMSClient(getAWSCredentials())
 	
 	Cryptor () {
 		sslkey = getSSLKey()
 		ssliv = getSSLIv()
+		Regions reg= Regions.fromName(getAWSRegion())
+		awsKmsClient.setRegion(Region.getRegion(reg))
+	}
+	
+	
+	static ByteBuffer str_to_bb(String msg, Charset charset){
+		return ByteBuffer.wrap(msg.getBytes(charset))
+	}
+	
+	static String bb_to_str(ByteBuffer buffer, Charset charset){
+		byte[] bytes;
+		if(buffer.hasArray()) {
+			bytes = buffer.array()
+		} else {
+			bytes = new byte[buffer.remaining()]
+			buffer.get(bytes)
+		}
+		return new String(bytes, charset);
 	}
 	
 	String  decrypt(String fileName)  {
@@ -28,13 +59,85 @@ class Cryptor   {
 	String getSSLKey() {
 		def sslkey = System.getenv('SSL_KEY')
 		if (!sslkey) {	sslkey = System.properties['SSL_KEY'] }
+		if (!sslkey) log.warn("No SSL_KEY found in environment")
 		return sslkey
 	}
 	
 	String getSSLIv() {
 		def ssliv = System.getenv('SSL_IV')
 		if (!ssliv) { ssliv = System.properties['SSL_IV'] }
+		if (!ssliv) log.warn("No SSL_IV found in environment")
 		return ssliv
+	}
+	
+	AWSCredentials getAWSCredentials() {
+		return new BasicAWSCredentials(getAWSAccessKeyId(),getAWSSecretAccessKey())
+	}
+	
+	ByteBuffer awsEncrypt(String data) {
+		EncryptRequest req = new EncryptRequest()
+		req.setKeyId(getAWSEncryptionKey() )
+		req.setPlaintext(str_to_bb(data, Charset.forName("UTF-8") ))
+
+		EncryptResult res = awsKmsClient.encrypt(req)
+		return res.getCiphertextBlob()
+	}
+	
+	String awsDecrypt(ByteBuffer data) {
+		DecryptRequest req = new DecryptRequest()
+		req.setCiphertextBlob(data)
+		DecryptResult res = awsKmsClient.decrypt(req)	
+		bb_to_str(res.plaintext,Charset.forName("UTF-8"))
+	}
+	
+	String getAWSRegion () {
+		def env = System.getenv('AWS_DEFAULT_REGION')
+		if (!env) {	env = System.properties['AWS_DEFAULT_REGION'] }
+		if (!env) log.warn("No AWS_DEFAULT_REGION found in environment")
+		return env
+	}
+	
+	String getAWSRegionUnderscore () {
+		String env = System.getenv('AWS_DEFAULT_REGION')
+		if (!env) {	env = System.properties['AWS_DEFAULT_REGION'] }
+		if (!env) log.warn("No AWS_DEFAULT_REGION found in environment")
+		env = env.replace("-","_")
+		return env
+	}
+	
+	String getAWSAccessKeyId () {
+		def sslkey = System.getenv('AWS_ACCESS_KEY_ID')
+		if (!sslkey) {	sslkey = System.properties['AWS_ACCESS_KEY_ID'] }
+		if (!sslkey) log.error("No AWS_ACCESS_KEY_ID found in environment")
+		return sslkey
+	}
+	
+	String getAWSSecretAccessKey () {
+		def sslkey = System.getenv('AWS_SECRET_ACCESS_KEY')
+		if (!sslkey) {	sslkey = System.properties['AWS_SECRET_ACCESS_KEY'] }
+		if (!sslkey) log.error("No AWS_SECRET_ACCESS_KEY found in environment")
+		return sslkey
+	}
+	
+	String getAWSEncryptionKey() {
+		String env = getEnv()
+		String envId
+		if (env == 'prod') envId = 'AWS_KMS_KEY'+"_"+getAWSRegionUnderscore()
+		else envId = 'AWS_KMS_KEY'+"_TEST_"+getAWSRegionUnderscore()
+		String key = System.getenv(envId)
+		if (!key) key = System.properties[envId]
+		if (!key)
+		  log.error("No encryptionKey "+envId+" found in environment")
+		else
+		  log.debug("Using encryptionKey environmnent variable: "+envId)
+		return key
+	}
+	
+	String getEnv() {
+		def env = System.getenv('ENV')
+		if (!env) {	env = System.properties['ENV'] }
+		if (!env) log.error("No ENV found in environment")
+		return env
 	}
 
 }
--- src/test/groovy/com/cloudburo/utility/CryptorTestCase.groovy
+++ src/test/groovy/com/cloudburo/utility/CryptorTestCase.groovy
@@ -3,16 +3,31 @@ package com.cloudburo.utility;
 import static org.junit.Assert.*;
 
 
+import java.nio.ByteBuffer
 import org.junit.Test;
 import org.junit.Before;
+import groovy.util.logging.Slf4j
 
+@Slf4j
 class CryptorTestCase {
 
-	//@Test
+	@Test
 	public void testDecrypt() {
-			Cryptor crypt = new Cryptor()
-			crypt.credentialsPath = "src/test/resources/"
-			String out = crypt.decrypt("test.user")
-			assert "talfco" == out
+		Cryptor crypt = new Cryptor()
+		crypt.credentialsPath = "src/test/resources/"
+		String out = crypt.decrypt("test.user")
+		assert "talfco" == out
 	}
+
+	@Test
+	public void awsTestKMS() {
+		log.info("awsTestKMS")
+		String data = "Hello World"
+		Cryptor crypt = new Cryptor()
+		ByteBuffer buf =  crypt.awsEncrypt(data)
+		String data1 = crypt.awsDecrypt(buf)
+		assert data == data1
+
+	}
+	
 }
